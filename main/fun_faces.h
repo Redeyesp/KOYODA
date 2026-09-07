bc8c47cfd438e73305b9f8fd47d0b1dc8ab14bd6
Peemmaphat <peemmaphat.buarapha@gmail.com>
2026-09-07 13:25:49 +0700

    Add files via upload

--- main/fun_faces.h
+++ main/fun_faces.h
@@ -2,9 +2,10 @@
 
 #include "lvgl.h"
 
-extern const lv_image_dsc_t fun_excited;
-extern const lv_image_dsc_t fun_tilt;
-extern const lv_image_dsc_t fun_drop;
-extern const lv_image_dsc_t fun_peak_thrill;
-extern const lv_image_dsc_t fun_dizzy;
+/*
+ * Remaining special full-screen faces:
+ * - fun_happy: used by Tap Wake, compiled into firmware.
+ * - fun_peak_thrill: reserved for future Gyro reaction, NOT compiled yet.
+ */
 extern const lv_image_dsc_t fun_happy;
+extern const lv_image_dsc_t fun_peak_thrill;
