261eaf185376c12b1994536425c46d117343b767
Peemmaphat Buarapha <Peemmaphat@tni.ac.th>
2026-09-11 16:24:35 +0700

    Use true eye-only blink without cheek pixels

--- main/koyoda_blink_patches.h
+++ main/koyoda_blink_patches.h
@@ -1,19 +1,18 @@
 #pragma once
-
 #include "lvgl.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-#define KOYODA_BLINK_LEFT_X 65
+#define KOYODA_BLINK_LEFT_X 58
 #define KOYODA_BLINK_LEFT_Y 121
-#define KOYODA_BLINK_LEFT_W 125
-#define KOYODA_BLINK_EYE_H 149
+#define KOYODA_BLINK_LEFT_W 137
+#define KOYODA_BLINK_EYE_H 148
 
-#define KOYODA_BLINK_RIGHT_X 278
+#define KOYODA_BLINK_RIGHT_X 271
 #define KOYODA_BLINK_RIGHT_Y 121
-#define KOYODA_BLINK_RIGHT_W 124
+#define KOYODA_BLINK_RIGHT_W 138
 
 extern const lv_image_dsc_t koyoda_blink_half_left;
 extern const lv_image_dsc_t koyoda_blink_half_right;
